fc12f801c9f597b394cce2e6711766e98e76dfff
“Tushar <tushar.kolhe01@gmail.com>
2018-04-14 16:29:51 +0530

    Start basic project

---
+++ TKLocationActivitiesTests/TKLocationActivitiesTests.m
@@ -0,0 +1,39 @@
+//
+//  TKLocationActivitiesTests.m
+//  TKLocationActivitiesTests
+//
+//  Created by Tushar Kolhe on 14/04/18.
+//  Copyright © 2018 Tushar Kolhe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TKLocationActivitiesTests : XCTestCase
+
+@end
+
+@implementation TKLocationActivitiesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
